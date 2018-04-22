de8d38159f744d6eb4f5e7fa45556677ca710e0e
dervinis <ddrdeivis@gmail.com>
2018-04-22 12:12:50 -0400

    Added Listview and Product Sample Information

--- platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,4 +1,35 @@
-$(document).on('pagecreate', '#home', function(){      
 
+$(document).on('pagecreate', '#home', function(){    
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+  
+    $prod = $("#prod");
+    var title = $prod.text(); // Will be dynamically updated
+    
+    function drawChart(title) {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Nutrition');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Calories', 200],
+          ['Fat', 0.1],
+          ['Sodium', 40],
+          ['Carbohydrate', 55],
+          ['Protein', 0.1]
+        ]);
+
+        // Set chart options
+        var options = {'title': 'Product',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
 });
   
